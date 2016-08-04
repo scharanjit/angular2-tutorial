cc2e452505b495b491c3cc75e3a360568a08d2c0
Charanjit Singh <charanjit.singh@imaginea.com>
2016-08-04 12:24:52 +0530

    Update gitignore pattern

--- app/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-// import { AppComponent } from './app.component';
-var components_1 = require('./components');
-platform_browser_dynamic_1.bootstrap(components_1.MyComponent);
-//# sourceMappingURL=main.js.map
